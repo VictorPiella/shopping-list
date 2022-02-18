8b800a3025eb3aa06ad416effb96aabe9c785e06
Victor Piella <vpiella28@gmail.com>
2022-02-18 16:49:27 +0100

    testing

--- src/app/components/products-list/products-list.component.spec.ts
+++ src/app/components/products-list/products-list.component.spec.ts
@@ -1,18 +1,14 @@
+import { render, screen } from '@testing-library/angular';
 import { ProductsListComponent } from './products-list.component';
-import { fireEvent, render, screen } from '@testing-library/angular';
-import { ReactiveFormsModule } from '@angular/forms';
 import userEvent from '@testing-library/user-event';
 
 it('should change the paramter of the search', async () => {
   const submitSpy = jest.fn();
   await render(ProductsListComponent, {
-      imports: [ReactiveFormsModule],
       componentProperties: {
         filterBy: 'title',
         changeFilter: submitSpy
       },
-      
-      
   });
 
   const priceBtn = screen.getByRole('button', {name: /price/i});
@@ -35,3 +31,53 @@ it('should change the paramter of the search', async () => {
 
 });
 
+it('should change the paramter of the search', async () => {
+  const submitSpy = jest.fn();
+  await render(ProductsListComponent, {
+      componentProperties: {
+        search: submitSpy
+      },
+  });
+
+  const input = screen.getByRole('button');
+  userEvent.type(input, 'pohone');
+  expect(submitSpy).toHaveBeenCalledWith({
+    search: 'iphone' 
+})
+  
+
+});
+
+
+it('should open favs modal', async () => {
+  await render(ProductsListComponent, {
+      componentProperties: {
+        modalFavs: false,
+      },
+  });
+
+  const favs = screen.getByAltText('favsModal');
+
+  userEvent.click(favs);
+  expect(modalFavs).toBeTruthy()
+
+});
+
+
+it('should add 5 more items to the list', async () => {
+  const submitSpy = jest.fn();
+  await render(ProductsListComponent, {
+      componentProperties: {
+        viewport: 4,
+        seeMore: submitSpy
+      },
+  });
+
+  const seeMore = screen.getByAltText('seeMore');
+
+  userEvent.click(seeMore);
+  expect(viewport).toBe(9);
+  expect(submitSpy).toHaveBeenCalled();
+  
+
+});
